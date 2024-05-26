8e2fbac619246800bb73fb5a9c54c00c3b968479
jaronnie <jaron@jaronnie.com>
2024-05-26 19:59:17 +0800

    feat(types): write types.go empty first

--- cmd/gen.go
+++ cmd/gen.go
@@ -23,9 +23,21 @@ func do(_ *cobra.Command, _ []string) error {
 	}
 
 	files, err := gen.Generate(p)
+	if err != nil {
+		return err
+	}
+
+	typesDir := filepath.Join(p.Dir, "internal", "types")
+	emptyTypesGoBytes, err := gen.ParseTemplate(nil, []byte(gen.TypesGoTpl))
+	if err != nil {
+		return err
+	}
+	if err = os.WriteFile(filepath.Join(typesDir, "types.go"), emptyTypesGoBytes, 0o644); err != nil {
+		return err
+	}
 
 	for _, v := range files {
-		typesGoFilePath := filepath.Join(p.Dir, "internal", "types", v.Path)
+		typesGoFilePath := filepath.Join(typesDir, v.Path)
 
 		if !pathx.FileExists(filepath.Dir(typesGoFilePath)) {
 			if err = os.MkdirAll(filepath.Dir(typesGoFilePath), 0o755); err != nil {
